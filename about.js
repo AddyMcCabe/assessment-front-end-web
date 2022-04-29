40d6624d9660023f4d8dcfd6e047a1922216df45
Addison McCabe <addisonhartmccabe@gmail.com>
2022-04-29 10:17:18 -0700

    initial commit

---
+++ about.js
@@ -0,0 +1,15 @@
+console.log("hello world");
+
+
+
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	alert('form submitted');
+}
+
+let catPic = document.getElementById('catPic');
+let form = document.querySelector('form#contact');
+
+form.addEventListener('submit', handleSubmit);
+catPic.addEventListener('mouseover', alert('Thank you for petting Mr Nice!') )
